b6fb52dba50d595a239f6c4db17ba5b467340bb9
Nikhil Kulkarni <nikhilskul7@gmail.com>
2023-05-22 17:58:10 +0530

    Completed assignment-2

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+         This is Nikhil's React WebApp!
         </p>
         <a
           className="App-link"
